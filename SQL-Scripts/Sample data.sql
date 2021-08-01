d89f641f4023a31066ea9fe6e77466f9b26b6fb9
Henrik Bartholdsson <henrik.bartholdsson@hotmail.com>
2021-08-01 18:04:20 +0200

    Adding sample data sql-script.

---
+++ SQL-Scripts/Sample data.sql
@@ -0,0 +1,9 @@
+select * from Authors
+
+
+INSERT INTO Authors (Id, Name, Contact)
+VALUES	(NEWID(),'Beng Olsson','bol@fgh.com'),
+		(NEWID(),'Anna-Maj Svensson','ams@dfasedf.se'),
+		(NEWID(),'Arne Andersson','aan@lasdk.com'),
+		(NEWID(),'Lisa Henriksson','lihen@sdfg.se'),
+		(NEWID(),'Efrahim','ef@sdf.com')
